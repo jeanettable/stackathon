e5fd60adff080664495b4542be6e04d03f42416c
Jeanettable <jeanetteabell@gmail.com>
2021-04-29 14:42:33 -0700

    BE : added prelim router usage middleware for viewing an individual profile

--- server/api/index.js
+++ server/api/index.js
@@ -9,6 +9,8 @@ router.use('/users/edit/:userId', isLoggedIn, isOwner, require('./users'));
 
 // lists and viewing all users is a production user accessible only piece of site
 router.use('/users', isLoggedIn, isProduction, require('./users'));
+// not sure how to deal with both production users and profile-owners being able to view profile
+router.use('/users/:userId', isLoggedIn, require('./users'));
 router.use('/lists', isLoggedIn, isProduction, isListOwner, require('./lists'));
 router.use('/lists/:listId', isLoggedIn, isProduction, isListOwner, require('./lists'));
 
